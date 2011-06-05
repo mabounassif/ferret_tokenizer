d20dfe0df667ccdc281bdc0f3537a70589c480a5
Mahmoud Abounassif <mahmoud.abounassif@gmail.com>
2011-06-04 20:19:16 -0700

    Edited ext/ferret_tokenizer.c via GitHub

--- ext/ferret_tokenizer.c
+++ ext/ferret_tokenizer.c
@@ -50,8 +50,8 @@ VALUE method_next(VALUE self) {
 }
 
 VALUE method_initialize(VALUE self, VALUE str) {
-	char* s = RSTRING(str)->ptr;
-	Tokenizer *t;
+  char* s = RSTRING_PTR(str);
+  Tokenizer *t;
   Data_Get_Struct(self, Tokenizer, t);
   t->data = s;
   return self;
